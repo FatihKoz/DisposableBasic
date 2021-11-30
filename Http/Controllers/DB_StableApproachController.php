2f5cf527501b41e6bba586f6c2bb7225c8606397
B.Fatih KOZ <fatih.koz@gmail.com>
2021-12-01 00:02:58 +0300

    Update DB_StableApproachController.php

--- Http/Controllers/DB_StableApproachController.php
+++ Http/Controllers/DB_StableApproachController.php
@@ -22,53 +22,67 @@ class DB_StableApproachController extends Controller
 
     public function store(Request $request)
     {
-        $status = (DB_Setting('dbasic.stable_app_control', false) === true) ? null : 'Plugin Support Disabled';
+        // Check Settings
+        $status = (DB_Setting('dbasic.stable_app_control', false) === true) ? null : 'Plugin support disabled';
 
         if (!isset($status)) {
+            // Check Contents
             $received_file = $request->file('report');
             $extension = $received_file->extension();
-
-            $status = ($extension != 'json') ? 'Report not in proper format ! Process aborted' : null;
+            $status = ($extension != 'json') ? 'Report not in proper format' : null;
         }
 
         if (!isset($status)) {
+            // Check Report Fields
             $report = DB_ReadSapReport($received_file->path());
-
-            $status = (isset($report->userID) && isset($report->plugin_version)) ? null : 'Report is not valid ! Process aborted';
-
-            if (isset($report) && !isset($status)) {
-                $requirements = is_array($report->requirementResultsGroups) ? collect($report->requirementResultsGroups) : null;
-                $is_stable = (isset($requirements) && $requirements->where('type', '2')->count()) ? false : true;
-            }
-
-            if (!isset($status)) {
-                $where = ['name' => DB_Setting('dbasic.stable_app_field', 'Stable Approach ID'), 'active' => true];
-                $field_id = DB::table('user_fields')->where($where)->value('id');
-                $user_id = DB::table('user_field_values')->where(['user_field_id' => $field_id, 'value' => $report->userID])->value('user_id');
-
-                $status = isset($user_id) ? null : 'No matching user found ! Process aborted';
-            }
-
-            if (!isset($status)) {
-                $pirep_id = DB::table('pireps')->where(['user_id' => $user_id, 'state' => 0])->orderBy('created_at', 'desc')->value('id');
-
-                $status = isset($pirep_id) ? null : 'No active pirep found ! Process aborted';
-            }
+            $status = (isset($report->userID) && isset($report->plugin_version) && isset($report->requirementResultsGroups)) ? null : 'Report is not valid';
         }
 
-        $new_report = isset($status) ? null : DB_StableApproach::updateOrCreate([
-            'sap_userID'     => $report->userID,
-            'sap_analysisID' => $report->analysis->id,
-        ], [
-            'user_id'        => $user_id,
-            'pirep_id'       => $pirep_id,
-            'is_stable'      => isset($is_stable) ? $is_stable : false,
-            'raw_report'     => json_encode($report),
-        ]);
+        if (!isset($status)) {
+            // Duplicate Report Check
+            $duplicate = DB_StableApproach::where('sap_analysisID', $report->analysis->id)->count();
+            $status = ($duplicate > 0) ? 'Already received' : null;
+        }
 
-        return view('DBasic::sap.received', [
-            'status' => isset($status) ? $status : 'OK',
-            'report' => $new_report,
-        ]);
+        if (!isset($status)) {
+            // Check Results
+            $requirements = is_array($report->requirementResultsGroups) ? collect($report->requirementResultsGroups) : null;
+            $is_stable = (isset($requirements) && $requirements->where('type', '2')->count()) ? false : true;
+        }
+
+        if (!isset($status)) {
+            // Check User
+            $where = ['name' => DB_Setting('dbasic.stable_app_field', 'Stable Approach ID'), 'active' => true];
+            $field_id = DB::table('user_fields')->where($where)->value('id');
+            $user_id = DB::table('user_field_values')->where(['user_field_id' => $field_id, 'value' => $report->userID])->value('user_id');
+            $status = isset($user_id) ? null : 'No matching user found.';
+        }
+
+        if (!isset($status)) {
+            // Check Pirep
+            $pirep_id = DB::table('pireps')->where(['user_id' => $user_id, 'state' => 0])->orderBy('created_at', 'desc')->value('id');
+            $status = isset($pirep_id) ? null : 'No active pirep found.';
+        }
+
+        if (!isset($status)) {
+            // Save the report
+            DB_StableApproach::updateOrCreate([
+                'sap_userID'     => $report->userID,
+                'sap_analysisID' => $report->analysis->id,
+            ], [
+                'user_id'        => $user_id,
+                'pirep_id'       => $pirep_id,
+                'is_stable'      => isset($is_stable) ? $is_stable : false,
+                'raw_report'     => json_encode($report),
+            ]);
+
+            $response['received'] = 'OK';
+        } else {
+            // Add reason to the response
+            $response['received'] = 'rejected';
+            $response['reason'] = $status;
+        }
+
+        return json_encode($response);
     }
 }
