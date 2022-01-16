1df1dd54bdc57cf968c579cb13d72194c7464cdf
B.Fatih KOZ <fatih.koz@gmail.com>
2022-01-16 14:42:14 +0300

    Update FlightBoard.php
    Eliminate "initiated" (prefiled by acars) but not started flights.

--- Widgets/FlightBoard.php
+++ Widgets/FlightBoard.php
@@ -5,6 +5,7 @@ namespace Modules\DisposableBasic\Widgets;
 use App\Contracts\Widget;
 use App\Models\Pirep;
 use App\Models\Enums\PirepState;
+use App\Models\Enums\PirepStatus;
 
 class FlightBoard extends Widget
 {
@@ -12,8 +13,8 @@ class FlightBoard extends Widget
 
     public function run()
     {
-        $eager_load = array('aircraft', 'airline', 'arr_airport', 'dpt_airport', 'position', 'user');
-        $flights = Pirep::with($eager_load)->where('state', PirepState::IN_PROGRESS)->orderby('updated_at', 'desc')->get();
+        $eager_load = ['aircraft', 'airline', 'arr_airport', 'dpt_airport', 'position', 'user'];
+        $flights = Pirep::with($eager_load)->where(['state' => PirepState::IN_PROGRESS, ['status', '!=', PirepStatus::INITIATED]])->orderby('updated_at', 'desc')->get();
 
         return view('DBasic::widgets.flight_board', [
             'flights'    => $flights,
