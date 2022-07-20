671397ed277a59459c1d12fda7a1f2fb8b6e838c
B.Fatih KOZ <fatih.koz@gmail.com>
2022-07-20 21:17:42 +0300

    Update 2022_07_20_174615_update_disposable_specs_table.php

--- Database/migrations/2022_07_20_174615_update_disposable_specs_table.php
+++ Database/migrations/2022_07_20_174615_update_disposable_specs_table.php
@@ -11,10 +11,10 @@ class UpdateDisposableSpecsTable extends Migration
         // Add new fields for Specs usage
         if (Schema::hasTable('disposable_specs')) {
             Schema::table('disposable_specs', function (Blueprint $table) {
-                $table->integer('selcal')->nullable()->after('crew');
-                $table->integer('hexcode')->nullable()->after('selcal');
-                $table->integer('rvr')->nullable()->after('hexcode');
-                $table->integer('rmk')->nullable()->after('rvr');
+                $table->string('selcal', 4)->nullable()->after('crew');
+                $table->string('hexcode', 6)->nullable()->after('selcal');
+                $table->string('rvr', 3)->nullable()->after('hexcode');
+                $table->string('rmk', 25)->nullable()->after('rvr');
             });
         }
     }
