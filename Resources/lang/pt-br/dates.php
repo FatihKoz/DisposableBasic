f6e0ec7680efa9404f42a0920da4ed08ae2bf1a8
B.Fatih KOZ <fatih.koz@gmail.com>
2022-11-13 17:52:57 +0300

    PT-BR Translation

---
+++ Resources/lang/pt-br/dates.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    // Month Names
+    '01' => 'Janeiro',
+    '02' => 'Fevereiro',
+    '03' => 'Março',
+    '04' => 'Abril',
+    '05' => 'Maio',
+    '06' => 'Junho',
+    '07' => 'Julho',
+    '08' => 'Agosto',
+    '09' => 'Setembro',
+    '10' => 'Outubro',
+    '11' => 'Novembro',
+    '12' => 'Dezembro',
+    // Day Names
+    'd1' => 'Segunda-feira',
+    'd2' => 'Terça-feira',
+    'd3' => 'Quarta-feira',
+    'd4' => 'Quinta-feira',
+    'd5' => 'Sexta-feira',
+    'd6' => 'Sábado',
+    'd7' => 'Domingo',
+    // Quarto Names
+    'q1' => '1. Quarto',
+    'q2' => '2. Quarto',
+    'q3' => '3. Quarto',
+    'q4' => '4. Quarto',
+];
