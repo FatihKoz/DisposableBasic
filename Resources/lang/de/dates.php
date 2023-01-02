986d13586e0f4505c301a29d7052baa8ddf752a8
B.Fatih KOZ <fatih.koz@gmail.com>
2023-01-02 20:13:18 +0300

    DE Translation
    Thanks to [Cyber Air](http://www.cyber-air.org/)

---
+++ Resources/lang/de/dates.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    // Month Names
+    '01' => 'Januar',
+    '02' => 'Februar',
+    '03' => 'März',
+    '04' => 'April',
+    '05' => 'Mai',
+    '06' => 'Juni',
+    '07' => 'Juli',
+    '08' => 'August',
+    '09' => 'September',
+    '10' => 'Oktober',
+    '11' => 'November',
+    '12' => 'Dezember',
+    // Day Names
+    'd1' => 'Montag',
+    'd2' => 'Tuestag',
+    'd3' => 'Mittwoch',
+    'd4' => 'Donnerstag',
+    'd5' => 'Freitag',
+    'd6' => 'Samstag',
+    'd7' => 'Sonntag',
+    // Quarter Names
+    'q1' => '1. Quartal',
+    'q2' => '2. Quartal',
+    'q3' => '3. Quartal',
+    'q4' => '4. Quartal',
+];
