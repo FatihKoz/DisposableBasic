927d4521e4f251cc8ac5c1d281bc05a053f4d5dd
B.Fatih KOZ <fatih.koz@gmail.com>
2024-11-15 21:56:49 +0300

    Turkish Translation Support

---
+++ Resources/lang/tr/dates.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    // Month Names
+    '01' => 'Ocak',
+    '02' => 'Şubat',
+    '03' => 'Mart',
+    '04' => 'Nisan',
+    '05' => 'Mayıs',
+    '06' => 'Haziran',
+    '07' => 'Temmuz',
+    '08' => 'Ağustos',
+    '09' => 'Eylül',
+    '10' => 'Ekim',
+    '11' => 'Kasım',
+    '12' => 'Aralık',
+    // Day Names
+    'd1' => 'Pazartesi',
+    'd2' => 'Salı',
+    'd3' => 'Çarşamba',
+    'd4' => 'Perşembe',
+    'd5' => 'Cuma',
+    'd6' => 'Cumartesi',
+    'd7' => 'Pazar',
+    // Quarter Names
+    'q1' => '1. Çeyrek',
+    'q2' => '2. Çeyrek',
+    'q3' => '3. Çeyrek',
+    'q4' => '4. Çeyrek',
+];
